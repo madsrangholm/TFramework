ba28102fa204e1468233f578dc032bd553b0c687
madsrangholm <madsrangholm@gmail.com>
2015-12-23 16:21:16 +0100

    Initial commit

---
+++ TFramework.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TFramework.Website.MvcApplication" Language="C#" %>
---
+++ TFramework.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TFramework.Website.Startup))]
+namespace TFramework.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
